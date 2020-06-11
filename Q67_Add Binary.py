83c401f9dd84e09bb214167c7bfb0349551637b1
Annie Chu <anniechu65@gmail.com>
2020-06-11 12:02:22 +0800

    Calculate the int with 2 and using join method to show the ans

--- Q67_Add Binary.py
+++ Q67_Add Binary.py
@@ -1,11 +1,23 @@
 def addBinary(a: str, b: str) -> str:
-    n1 = int(a, 2)
-    n2 = int(b, 2)
+    n1 = len(a) - 1
+    n2 = len(b) - 1
+    cal = 0
+    ans = []
 
-    n3 = bin(n1 + n2)[2:]
+    while n1 >= 0 or n2 >= 0 or cal is not 0:
+        if n1 >= 0:
+            cal += int(a[n1])
+            n1 -= 1
 
-    print(n3)
+        if n2 >= 0:
+            cal += int(b[n2])
+            n2 -= 1
+
+        ans.append(str(cal % 2))
+        cal //= 2
+
+    print(''.join(reversed(ans)))
 
 
 if __name__ == '__main__':
-    addBinary('11', '1')
+    addBinary('1010', '1011')
